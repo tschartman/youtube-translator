ba7f26e8549436dd9c768d0678a181db8d67a6d9
tim <tschartman@yahoo.com>
2024-01-06 12:50:56 -0600

    Initialize youtube translator api

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { getSubtitles } from './parser';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/captions', async (req, res) => {
+
+  const {youtubeUrl} = req.body;
+  if (!youtubeUrl) {
+    res.status(400).send('youtubeUrl is required');
+  } else {
+    const data = await getSubtitles({videoID: youtubeUrl, lang: 'en'})
+    const script = data.map((obj: { text: string; }) => {
+      return obj.text
+    }).join(" ")
+    res.status(200).send(script)
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
